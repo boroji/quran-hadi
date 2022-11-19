28065f9b8b5689d56189561359629d7fd851d747
Persian Delight <hosseinboroojinejadshirazi@gmail.com>
2022-11-19 16:22:27 -0500

    button style added

---
+++ components/UI/Button/Button.js
@@ -0,0 +1,44 @@
+import styles from './Button.module.scss'
+import { useGlobalContext } from '../../../utils/GlobalAppState'
+import { Noto_Sans_Arabic } from '@next/font/google'
+
+const ArabicFont = Noto_Sans_Arabic({
+	variable: '--noto-sans',
+	fallback: ['Noto Sans Arabic', 'Vazirmatn'],
+	display: 'block',
+})
+
+const Button = ({
+	id,
+	className,
+
+	type = 'primary',
+
+	text,
+	clickHandler,
+	iconLeft,
+	iconRight,
+
+	refHandler,
+}) => {
+	const { dark } = useGlobalContext()
+
+	const { btn, primaryLight, primaryDark, secondaryLight, secondaryDark } = styles
+
+	const isPrimaryDark = `${dark ? primaryDark : primaryLight}`
+	const isSecondaryDark = `${dark ? secondaryDark : secondaryLight}`
+
+	const buttonType = `${type == 'primary' ? isPrimaryDark : isSecondaryDark}`
+
+	const dynamicClassName = `${ArabicFont.variable} ${btn} ${buttonType} ${className ? className : ''}`
+
+	return (
+		<div dir='rtl' id={id} className={dynamicClassName} onClick={clickHandler} ref={refHandler}>
+			{iconLeft && iconLeft}
+			{text}
+			{iconRight && iconRight}
+		</div>
+	)
+}
+
+export default Button
