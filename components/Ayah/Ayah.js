8df123394c6c5399992bb06e2b03de4bb2fdd7c8
Persian Delight <hosseinboroojinejadshirazi@gmail.com>
2022-12-05 18:31:55 -0500

    added each ayah in the respective line

--- components/Ayah/Ayah.js
+++ components/Ayah/Ayah.js
@@ -1,12 +1,17 @@
 import Kalameh from '@components/Kalameh/Kalameh'
 import Tarjomeh from '@components/Tarjomeh/Tarjomeh'
+import { useGlobalContext } from '@utils/GlobalAppState'
 import styles from './Ayah.module.scss'
 
 const Ayah = ({ text }) => {
-	const { container, words } = styles
+	const { dark } = useGlobalContext()
+
+	const { container, containerDark, containerLight, words } = styles
+
+	const isDark = dark ? containerDark : containerLight
 
 	return (
-		<section className={container}>
+		<section className={`${container} ${isDark}`}>
 			{text.map(({ id, kalameh, tarjomeh }) => {
 				return (
 					<article key={id} className={words}>
